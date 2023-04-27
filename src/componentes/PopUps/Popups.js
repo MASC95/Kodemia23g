be97ab0097671df2c1a0310c2322bd76a8d146ae
MASC95 <miguelangel.serapio@iest.edu.mx>
2023-04-27 15:23:36 -0600

    prueba de pop-ups

---
+++ src/componentes/PopUps/Popups.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Popups = () => {
+  return (
+    <div className='notification'>
+        <p>Esto es un pop up creado con React y Bootstrap V!👻👻</p>
+        <span className='notification__progress'></span>
+    </div>
+  )
+}
+
+export default Popups

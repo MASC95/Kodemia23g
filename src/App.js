b838e96ca8277594a3683b42531b28628863a143
MASC95 <miguelangel.serapio@iest.edu.mx>
2023-04-27 15:43:25 -0600

    testing pop-ups

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import Navbar from './componentes/Navbar';
+import Popups from './componentes/PopUps/Popups';
 import Table1 from './componentes/Table1b/Table1';
 
 function App() {
   return (
     <div className="App">
-     <Table1/>
+     <Popups/>
     </div>
   );
 }
--- src/componentes/PopUps/Popups.js
+++ src/componentes/PopUps/Popups.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import "./popups.scss";
 
 const Popups = () => {
   return (

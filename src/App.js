4c7d3622c0855d3bcc68adce648f06c836bdff6f
Cinthia <cinthiasanchezr@1997@gmail.com>
2023-04-27 19:17:36 -0700

    formGral

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import './App.css';
 import Navbar from './componentes/Navbar';
+import FormProfile from './componentes/FormProfile/FormProfile';
 
 function App() {
   return (
-    <div className="App">
-      <Navbar/>
+    // <div className="App">
+    //   <Navbar/>
+    // </div>
+    <>
+    <div className='container-fluid'>
+      <div className='row'>
+        <div className='col-3 bg-primary'> SIDE BAR MENU</div>
+        <div className='col-9'>
+          <FormProfile/>
+        </div>
+      </div>
     </div>
+    </>
   );
 }
 
--- src/componentes/FormProfile/FormProfile.js
+++ src/componentes/FormProfile/FormProfile.js
@@ -3,6 +3,79 @@ import React from "react";
 export const FormProfile=()=>{
     return(
         <>
+        <div className="container mt-2 p-5 w-100">
+            <h1>Información General</h1>
+        <form>
+            <div className="row mb-4">
+                <div className="col">
+                <div className="form-outline bg-gray">
+                    <input type="text" id="form6Example1" className="form-control"  />
+                    <label className="form-label" for="form6Example1">Nombre</label>
+                </div>
+                </div>
+                <div className="col">
+                <div className="form-outline">
+                    <input type="text" id="form6Example2" className="form-control" />
+                    <label className="form-label" for="form6Example2">Apellido</label>
+                </div>
+                </div>
+            </div>
+            <div className="row mb-4">
+                <div className="col">
+                <div className="form-outline">
+                    <input type="text" id="form6Example1" className="form-control" />
+                    <label className="form-label" for="form6Example1">Edad</label>
+                </div>
+                </div>
+                <div className="col">
+                <div className="form-outline">
+                    <input type="text" id="form6Example2" className="form-control" />
+                    <label className="form-label" for="form6Example2">Genero</label>
+                </div>
+                </div>
+            </div>
+            <div className="row mb-4">
+                <div className="col">
+                <div className="form-outline">
+                    <input type="text" id="form6Example1" className="form-control" />
+                    <label className="form-label" for="form6Example1">Nivel de Estudios</label>
+                </div>
+                </div>
+                <div className="col">
+                <div className="form-outline">
+                    <input type="text" id="form6Example2" className="form-control" />
+                    <label className="form-label" for="form6Example2">Experiencia</label>
+                </div>
+                </div>
+            </div>
+            <div className="row mb-4">
+                <div className="col">
+                <div className="form-outline">
+                    <input type="text" id="form6Example1" className="form-control" />
+                    <label className="form-label" for="form6Example1">Email</label>
+                </div>
+                </div>
+                <div className="col">
+                <div className="form-outline">
+                    <input type="text" id="form6Example2" className="form-control" />
+                    <label className="form-label" for="form6Example2">Contraseña</label>
+                </div>
+                </div>
+            </div>
+            <div className="buttons_actions justify-content-around">
+            <div className="row">
+                <div className="col-8">
+                </div>
+                <div className="col-4 d-flex justify-content-around">
+                 <button type="button" class="btn btn-info">Cancelar</button>
+                <button type="button" class="btn btn-outline-info">Crear</button>
+                </div>
+            </div>
+               
+            </div>
+        </form>
+        </div>
+
         </>
     )
 }

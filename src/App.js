a49d2bbe7ed7ca4954cfc2e67535bfdbef69cf4f
MASC95 <miguelangel.serapio@iest.edu.mx>
2023-05-01 17:28:54 -0600

    adding window vacante modificada

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import './App.css';
 import Navbar from './componentes/Navbar';
 import Popups from './componentes/PopUps/Popups';
 import Table1 from './componentes/Table1b/Table1';
-
+import Window from './componentes/Window/Window';
 function App() {
   return (
     <div className="App">
       
-     <Popups/>
+     <Window/>
     </div>
   );
 }
--- src/componentes/Window/Window.js
+++ src/componentes/Window/Window.js
@@ -16,7 +16,7 @@ const Window = () => {
       <div className="modal-dialog">
         <div className="modal-content">
           <div className="modal-header">
-            <h5 className="modal-title" id="exampleModalLabel">Modal title</h5>
+            <h5 className="modal-title" id="exampleModalLabel">Vacante Modificada</h5>
             <button type="button" className="btn-close" data-dismiss="modal" aria-label="Close"></button>
           </div>
           <div className="modal-body">...</div>

b2099aa6d80df7a805e04d6b06c3144aaaba8c13
MASC95 <miguelangel.serapio@iest.edu.mx>
2023-05-02 13:34:51 -0600

    modal application accepted - done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import './App.css';
-import Navbar from './componentes/Navbar';
-import Popups from './componentes/PopUps/Popups';
-import Table1 from './componentes/Table1b/Table1';
+
 import Window from './componentes/Window/Window';
 function App() {
   return (
--- src/componentes/Window/Window.js
+++ src/componentes/Window/Window.js
@@ -1,34 +1,40 @@
-import React, { useState } from "react";
+import React from 'react';
+import { Button, Modal, Container, Row, Col } from 'react-bootstrap';
+import { IoCheckmarkCircleSharp } from 'react-icons/io5';
 
 const Window = () => {
-     
-        const [show, setShow] = useState(false);
-      
-        const handleClose = () => setShow(false);
-        const handleShow = () => setShow(true);
+  const [showModal, setShowModal] = React.useState(false);
+
+  const handleCloseModal = () => setShowModal(false);
+  const handleOpenModal = () => setShowModal(true);
+
   return (
-    <>
-    <button type="button" className="btn btn-success" data-toggle="modal" data-target="#exampleModal">
-      Launch demo modal
-    </button>
+    <Container>
+      <Row >
+        <Col className="">
+         
+          <Button variant="danger" size="lg" onClick={handleOpenModal} >
+            Click to open Modal
+          </Button>
+        </Col>
+      </Row>
 
-    <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-      <div className="modal-dialog">
-        <div className="modal-content">
-          <div className="modal-header">
-            <h5 className="modal-title" id="exampleModalLabel">Vacante Modificada</h5>
-            <button type="button" className="btn-close" data-dismiss="modal" aria-label="Close"></button>
-          </div>
-          <div className="modal-body">...</div>
-          <div className="modal-footer">
-            <button type="button" className="btn btn-secondary" data-dismiss="modal" onClick={handleClose}>Close</button>
-            <button type="button" className="btn btn-primary" onClick={() => {}}>Save changes</button>
-          </div>
-        </div>
-      </div>
-    </div>
-  </>
-  )
-}
+      <Modal show={showModal} onHide={handleCloseModal} aria-labelledby="basicModal">
+        <Modal.Header closeButton>
+          <Modal.Title id="myModalLabel">Close</Modal.Title>
+        </Modal.Header>
 
-export default Window
+        <Modal.Body className=''>
+          <h3 className='d-flex justify-content-center'>Aplicación Aceptada</h3>
+          <div className='icon d-flex justify-content-center mb-4'>
+          <IoCheckmarkCircleSharp style={{width:'200px', height: '200px', color: '#1DD75B' }}/>
+          </div>
+        </Modal.Body>
+
+        
+      </Modal>
+    </Container>
+  );
+};
+
+export default Window;

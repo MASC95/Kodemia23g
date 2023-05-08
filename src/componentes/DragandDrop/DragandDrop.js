6e936ab0eea027516dafc9f5dd74c9c476721bf7
MASC95 <miguelangel.serapio@iest.edu.mx>
2023-05-07 21:14:43 -0600

    drag and drop completed

--- src/componentes/DragandDrop/DragandDrop.js
+++ src/componentes/DragandDrop/DragandDrop.js
@@ -1,38 +1,34 @@
 import React from "react";
 import { useState } from "react";
 import "./draganddrop.scss";
+import { BsThreeDots } from "react-icons/bs";
 
 const DragandDrop = () => {
     
   const [tasks, setTasks] = useState([
     {
       id: 1,
-      title: "Tarea 1",
-      body: "Lorem, ipsum dolor sit amet consectetur adipisicing elit ipsum dolor.",
+      body: "Candidato 1",
       list: 1,
     },
     {
       id: 2,
-      title: "Tarea 2",
-      body: "Lorem, ipsum dolor sit amet consectetur adipisicing elit ipsum dolor.",
+      body: "Candidato 2",
       list: 1,
     },
     {
       id: 3,
-      title: "Tarea 3",
-      body: "Lorem, ipsum dolor sit amet consectetur adipisicing elit ipsum dolor.",
+      body: "Candidato 3",
       list: 3,
     },
     {
       id: 4,
-      title: "Tarea 4",
-      body: "Lorem, ipsum dolor sit amet consectetur adipisicing elit ipsum dolor.",
+      body: "Candidato 4",
       list: 2,
     },
     {
       id: 5,
-      title: "Tarea 5",
-      body: "Lorem, ipsum dolor sit amet consectetur adipisicing elit ipsum dolor.",
+      body: "Candidato 5",
       list: 2,
     },
   ]);
@@ -64,11 +60,12 @@ const onDrop = (evt, list) => {
 
   return (
     <div className="main">
-      <h1>Drag and Drop</h1>
+      <h1>Nombre de la vacante: Developer lista de
+candidatos</h1>
       <br />
       <div className="drag-and-drop">
         <div className="column column--1">
-          <h3>Tareas por Hacer</h3>
+          <h3>Primer Contacto <BsThreeDots className="mx-3"/></h3> 
           <div
             className="dd-zone"
             droppable="true" onDragOver={(evt => draggingOver(evt))} onDrop={(evt => onDrop(evt, 1))}
@@ -78,14 +75,14 @@ const onDrop = (evt, list) => {
                 className="dd-element"
                 key={item.id} draggable onDragStart={(evt) => startDrag(evt, item)}
               >
-                <strong className="title">{item.title} </strong>
+                
                 <p className="body">{item.body}</p>
               </div>
             ))}
           </div>
         </div>
         <div className="column column--2">
-          <h3>Tareas en Progreso</h3>
+          <h3>Entrevista <BsThreeDots className="mx-3"/></h3>
           <div
             className="dd-zone zona2"
             droppable="true"
@@ -97,21 +94,35 @@ const onDrop = (evt, list) => {
                 className="dd-element elem2"
                 key={item.id} draggable onDragStart={(evt) => startDrag(evt, item)} 
               >
-                <strong className="title titulo2">{item.title}</strong>
+                
                 <p className="body body2">{item.body}</p>
               </div>
             ))}
           </div>
         </div>
-        <div className="column column-3">
+        <div className="column column--3">
             <h3>
-                Tareas Realizadas
+              Pruebas <BsThreeDots className="mx-3"/>
             </h3>
             <div className="dd-zone zona3" droppable="true" onDragOver={(evt => draggingOver(evt))} onDrop={(evt => onDrop(evt, 3))}>
             {getList(3).map(item => (
                             <div className='dd-element elem3' key={item.id} draggable onDragStart={(evt) => startDrag(evt, item)}>
-                                <strong className='title titulo3'>{item.title}</strong>
+                                
                                 <p className='body body3'>{item.body}</p>
+                            </div>
+                        ))}
+
+            </div>
+        </div>
+        <div className="column column--4">
+            <h3>
+              Contratado <BsThreeDots className="mx-3"/>
+            </h3>
+            <div className="dd-zone zona4" droppable="true" onDragOver={(evt => draggingOver(evt))} onDrop={(evt => onDrop(evt, 4))}>
+            {getList(4).map(item => (
+                            <div className='dd-element elem4' key={item.id} draggable onDragStart={(evt) => startDrag(evt, item)}>
+                                
+                                <p className='body body4'>{item.body}</p>
                             </div>
                         ))}
 

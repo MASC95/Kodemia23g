5b0c89da40f0ff171b60797fcdbd3412f3f5906d
MASC95 <miguelangel.serapio@iest.edu.mx>
2023-05-11 19:38:09 -0600

    forms

--- src/componentes/Candidate/ProfileCandidate/Form/FormRecruiter.js
+++ src/componentes/Candidate/ProfileCandidate/Form/FormRecruiter.js
@@ -3,8 +3,65 @@ import imgProfile from "../../../Recruiter/assets/img/perfil2.jpg";
 import "../scss/style.scss";
 import { UseForm } from "../../hooks/useForm";
 
-const initialForm = {};
-const validationsForm = (form) => {};
+const initialForm = {
+    name: "",
+    lastName: "",
+    password: "",
+    age: "",
+    exp: "",
+};
+
+const validationsForm = (form) => {
+    let errors = {};
+    let regexName = /^[A-Za-zÑñÁáÉéÍíÓóÚúÜü\s]+$/;
+    let regexLastName = /^[A-Za-zÑñÁáÉéÍíÓóÚúÜü\s]+$/;
+    let regexEmail = /^(\w+[/./-]?){1,}@[a-z]+[/.]\w{2,}$/;
+    let regexAge = /^\d+$/;
+    let regexPassword = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
+    let regexExp = /^.{1,255}$/;
+
+    if (!form.name.trim()) {
+        errors.name = "El campo 'Nombre' es obligatorio";
+    } else if(!regexName.test(form.name.trim())){
+        errors.name = "El campo 'Nombre' debe contener solo letras y espacios en blanco";
+    }
+
+    if (!form.lastname.trim()) {
+        errors.lastName = "El campo 'Apellido' es obligatorio";
+    } else if (!regexLastName.test(form.lastName.trim())){
+        errors.lastName = "El campo 'Apellido' debe contener solo letras y espacios en blanco";
+    }
+
+    if (!form.password.trim()) {
+        errors.password = "El campo 'email' es obligatorio";
+    }
+    else if (!regexPassword.test(form.password.trim())){
+        errors.password = "Las contraseñas no coinciden "
+    }
+
+    if (!form.age.trim()) {
+        errors.age = "El campo 'edad' es obligatorio";
+    }
+    else if (!regexAge.test(form.test.trim())){
+        errors.age = "El campo 'edad' debe contener solo numeros y espacios en blanco";
+    }
+
+    if (!form.exp.trim()) {
+        errors.exp = "El campo 'experiencia' es obligatorio";
+    }
+    else if (!regexExp.test(form.test.trim())){
+        errors.exp = "El campo 'experiencia' debe contener solo numeros y espacios en blanco";
+    }
+    if (!form.email.trim()){
+        errors.email = "El campo 'email' es obligatorio";
+    }
+    else if (!regexEmail.test(form.test.trim())){
+        errors.email = "El campo 'email' debe contener un email valido";
+    }
+
+
+    return errors
+};
 export const FormRecruiter = () => {
   const {
     form,
@@ -53,9 +110,10 @@ export const FormRecruiter = () => {
                     required
                   />
                 </form>
+                {errors.name && <div className="alert alert-warning d-flex justify-content-center" role="alert" style={{fontSize: '12px', fontWeight:'bold'}} >{errors.name}</div>}
               </div>
               <div className="col">
-                <div className="form-outline">
+                <form className="form-outline">
                   <label className="form-label" for="form6Example1">
                     Apellido
                   </label>
@@ -70,12 +128,13 @@ export const FormRecruiter = () => {
                     value={form.lastName}
                     required
                   />
-                </div>
+                </form>
+                {errors.lastName && <div className="alert alert-warning d-flex justify-content-center" role="alert" style={{fontSize: '12px', fontWeight:'bold'}} >{errors.lastName}</div>}
               </div>
             </div>
             <div className="row mb-4">
               <div className="col">
-                <div className="form-outline bg-gray">
+                <form className="form-outline bg-gray">
                   <label className="form-label" for="form6Example1">
                     Edad:
                   </label>
@@ -90,10 +149,11 @@ export const FormRecruiter = () => {
                     value={form.age}
                     required
                   />
-                </div>
+                </form>
+                {errors.age && <div className="alert alert-warning d-flex justify-content-center" role="alert" style={{fontSize: '12px', fontWeight:'bold'}} >{errors.age}</div>}
               </div>
               <div className="col">
-                <div className="form-outline">
+                <form className="form-outline">
                   <label className="form-label" for="form6Example1">
                     Escolaridad
                   </label>
@@ -103,7 +163,7 @@ export const FormRecruiter = () => {
                     <option>Licenciatura</option>
                     <option>Carrera Técnica</option>
                   </select>
-                </div>
+                </form>
               </div>
             </div>
             <div className="row mb-4">
@@ -124,6 +184,7 @@ export const FormRecruiter = () => {
                     required
                   />
                 </form>
+                {errors.email && <div className="alert alert-warning d-flex justify-content-center" role="alert" style={{fontSize: '12px', fontWeight:'bold'}} >{errors.email}</div>}
               </div>
               <div className="col">
                 <form className="form-outline">
@@ -142,6 +203,7 @@ export const FormRecruiter = () => {
                     required
                   />
                 </form>
+                {errors.password && <div className="alert alert-warning d-flex justify-content-center" role="alert" style={{fontSize: '12px', fontWeight:'bold'}} >{errors.password}</div>}
               </div>
             </div>
             <div className="row mb-4">
@@ -162,6 +224,7 @@ export const FormRecruiter = () => {
                     required
                   />
                 </form>
+                {errors.exp && <div className="alert alert-warning d-flex justify-content-center" role="alert" style={{fontSize: '12px', fontWeight:'bold'}} >{errors.exp}</div>}
               </div>
             </div>
             <div className="buttons_actions d-flex justify-content-center gap-3">

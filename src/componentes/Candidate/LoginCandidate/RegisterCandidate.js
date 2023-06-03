a9376c7a6fc900e074aa1c89546387c42bee10fd
Ramiro Ocampo Rodriguez <101475348+ROR2022@users.noreply.github.com>
2023-06-03 16:20:53 -0600

    cambios devR

--- src/componentes/Candidate/LoginCandidate/RegisterCandidate.js
+++ src/componentes/Candidate/LoginCandidate/RegisterCandidate.js
@@ -1,87 +1,106 @@
-import {useState} from "react";
-import logo from './img/logo.png'
-import register from './img/14.png'
-import './scss/style.scss'
+import { useEffect, useState } from "react";
+import logo from "./img/logo.png";
+import register from "./img/14.png";
+import "./scss/style.scss";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
 import swal from "sweetalert";
 import { endpointsGral } from "../../Recruiter/services/vacancy";
 
-export const RegisterCandidate=()=>{
-  const navigate =useNavigate()
-  const [formValues, setFormValues]=useState({
-    email:'',
-    password:'',
-    role:''
-  })
-  const onFormInputChange=(event)=>{
-    const Input=event.target.id;
-    const InputValue=event.target.value;
+export const RegisterCandidate = () => {
+  const [isResgitering, setIsResgitering] = useState(false);
+  const [isConfirmEmail, setIsConfirmEmail] = useState(false);
+  const navigate = useNavigate();
+  const [formValues, setFormValues] = useState({
+    email: "",
+    password: "",
+    role: "",
+    code: "",
+    backCode:'',
+    rfc: "",
+  });
+
+  useEffect(() => {
+    if(formValues.code!==''&&(formValues.code===String(formValues.backCode))){
+      setIsConfirmEmail(true);
+      
+    }else{
+      setIsConfirmEmail(false);
+    }
+    
+  }, [formValues.code,formValues.backCode])
+  
+  const onFormInputChange = (event) => {
+    const Input = event.target.id;
+    const InputValue = event.target.value;
 
     setFormValues({
-      ...formValues,[Input]:InputValue
-    })
-  }
+      ...formValues,
+      [Input]: InputValue,
+    });
+  };
 
-  const onFormSubmit=(event)=>{
-    event.preventDefault()
-    registerRecruiter()
-  }
-  const importantData= (formValues.email!=='' &&
-                        formValues.role !==''&&
-                        formValues.password!=='')
-  
-  const resetForm=()=>{
-        setFormValues({
-          email:'',
-          password:'',
-          role:''
-          })
-      }
+  const onFormSubmit = (event) => {
+    event.preventDefault();
 
-  const registerRecruiter=async()=>{
-    if(formValues.role==='candidato'){
-      try {
-      console.log(formValues)
-      if(importantData){
-        const register=await axios.post(endpointsGral.userURL,formValues);
-        setFormValues(register)
-        swal({
-          title: "Bienvenido!",
-          icon: "success",
-          button: "ok!",
-        });
-        resetForm()
-        console.log('pagina candidato')
-      } else{
-        swal({
-          title: "Todos los campos son requeridos!",
-          icon: "error",
-          button: "ok!",
-        });
-      }
+    setIsResgitering(true);
+
+    confirmAccesCode();
+
+   
+  };
+  const importantData =
+    formValues.email !== "" &&
+    formValues.role !== "" &&
+    formValues.password !== "";
+
+  const resetForm = () => {
+    setFormValues({
+      email: "",
+    password: "",
+    role: "",
+    code: "",
+    backCode:'',
+    rfc: "",
+    });
+  };
+
+  const confirmAccesCode = async () => {
+    console.log("Estamos confirmando el email:..");
+    const { confirmEmail } = endpointsGral;
+    const dataLogin = {
+      email: formValues.email,
+    };
+    try {
+      const response = await axios.post(confirmEmail, dataLogin);
+      console.log("responseConfirmEmail:..", response);
+      setFormValues({
+        ...formValues,
+        backCode:response?.data?.code
+      })
     } catch (error) {
-      swal({
-        title: "Error al registrar!",
-        icon: "error",
-        button: "ok!",
-      });
+      console.log(error);
     }
-    }else{
+
+    
+  };
+
+  const registerRecruiter = async () => {
+    if (formValues.role === "candidato") {
       try {
-        console.log(formValues)
-        if(importantData){
-          const register=await axios.post(endpointsGral.userURL,formValues);
-          setFormValues(register)
-          swal({
+        console.log(formValues);
+        if (importantData) {
+          const register = await axios.post(endpointsGral.registerUser, formValues);
+          setFormValues(register);
+          /* swal({
             title: "Bienvenido!",
             icon: "success",
             button: "ok!",
-          });
-          resetForm()
-          console.log('pagina empresa')
-        //  navigate(`/Dashboard-recruiter/home`)
-        } else{
+          }); */
+          resetForm();
+          console.log("pagina candidato");
+          navigate(`/dashboard-candidato/home`)
+        } else {
           swal({
             title: "Todos los campos son requeridos!",
             icon: "error",
@@ -89,83 +108,156 @@ export const RegisterCandidate=()=>{
           });
         }
       } catch (error) {
-        
+        swal({
+          title: "Error al registrar!",
+          icon: "error",
+          button: "ok!",
+        });
       }
+    } else {
+      try {
+        console.log(formValues);
+        if (importantData) {
+          const register = await axios.post(endpointsGral.userURL, formValues);
+          setFormValues(register);
+          /* swal({
+            title: "Bienvenido!",
+            icon: "success",
+            button: "ok!",
+          }); */
+          resetForm();
+          console.log("pagina empresa");
+          navigate(`/Dashboard-recruiter/home`)
+        } else {
+          swal({
+            title: "Todos los campos son requeridos!",
+            icon: "error",
+            button: "ok!",
+          });
+        }
+      } catch (error) {}
     }
-  }
+  };
 
-    return(
-        <>
-        <section className="signin-page account">
-          <div className="container">
-            <div className="row">
-              <div className="col-md-6 col-md-offset-3">
-                <div className="block text-center">
-                  <Link to={'/'}>
-                    <a className="logo_Jobinder" href="#!">
-                        <img src={logo} alt=""/>
-                    </a>
-                  </Link>
+  const handleConfirmEmail = () => {
+    console.log("codigo:", formValues.code);
+    if(isConfirmEmail===true){
+      registerRecruiter();
+      console.log('Email confirmado con Exito:..');
+    }else{
+      console.log('Codigo de acceso Erroneo:..');
+    }
+  };
+
+  return (
+    <>
+      <section className="signin-page account">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-6 col-md-offset-3">
+              <div className="block text-center">
+                <Link to={"/"}>
+                  <img src={logo} alt="" />
+                </Link>
                 <h2 className="text-center text-dark">Bienvenido</h2>
                 <form className="text-left clearfix" onSubmit={onFormSubmit}>
-                    <div className="form-group">
-                    <input type="email" 
-                           value={formValues.email}
-                           onChange={onFormInputChange}
-                           className="form-control" 
-                           id="email" 
-                           placeholder="Email"/>
-                           
-                    </div>
-                    <div className="form-group">
-                            {/* <label className="form-label" for="form6Example1">Role</label> */}
-                            <select className="form-control" id="role" value={formValues.role} onChange={onFormInputChange} >
-                                <option value="" selected disabled>Rol</option>
-                                <option value='candidato'>candidato</option>
-                                <option value='empresa'>empresa</option>
-                            </select>
+                  <div className="form-group">
+                    <input
+                      type="email"
+                      value={formValues.email}
+                      onChange={onFormInputChange}
+                      className="form-control"
+                      id="email"
+                      placeholder="Email"
+                    />
+                  </div>
+                  <div className="form-group">
+                    {/* <label className="form-label" for="form6Example1">Role</label> */}
+                    <select
+                      className="form-control"
+                      id="role"
+                      value={formValues.role}
+                      onChange={onFormInputChange}
+                    >
+                      <option value="">Rol</option>
+                      <option value="candidato">candidato</option>
+                      <option value="empresa">empresa</option>
+                    </select>
+                  </div>
+                  <div className="form-group">
+                    <input
+                      type="text"
+                      value={formValues.rfc}
+                      onChange={onFormInputChange}
+                      className="form-control"
+                      id="rfc"
+                      placeholder="RFC"
+                    />
+                  </div>
+                  <div className="form-group">
+                    <input
+                      type="password"
+                      value={formValues.password}
+                      onChange={onFormInputChange}
+                      className="form-control"
+                      id="password"
+                      placeholder="Password"
+                    />
+                  </div>
+                  <div className="text-center">
+                    {!isResgitering && (
+                      <div className="buttons_actions d-grid">
+                        <button
+                          type="submit"
+                          className="buttons btn btn-info btn-lg"
+                        >
+                          Enviar
+                        </button>
+                      </div>
+                    )}
+
+                    {isResgitering && (
+                      <>
+                        <label className="text-dark" htmlFor="code">
+                          Captura el código que fue enviado a tu E-mail:
+                        </label>
+                        <input
+                          type="text"
+                          value={formValues.code}
+                          id="code"
+                          onChange={onFormInputChange}
+                          className="form-control"
+                          placeholder="codigo de acceso"
+                        />
+                        <div className="buttons_actions d-grid">
+                          <button
+                            type="button"
+                            onClick={handleConfirmEmail}
+                            className="buttons btn btn-info btn-lg"
+                          >
+                            Confirmar
+                          </button>
                         </div>
-                    <div className="form-group">
-                    <input type="text" 
-                           value={formValues.rfc}
-                           onChange={onFormInputChange}
-                           className="form-control" 
-                           id="rfc" 
-                           placeholder="RFC"/>
-                    </div>
-                    <div className="form-group">
-                    <input type="password" 
-                           value={formValues.password}
-                           onChange={onFormInputChange}
-                           className="form-control" 
-                           id="password" 
-                           placeholder="Password"/>
-                    </div>
-                    <div className="text-center">
-                    <div className="buttons_actions d-grid">  
-                        <button type="submit" className="buttons btn btn-info btn-lg">Enviar</button>               
-                    </div>
+                      </>
+                    )}
                   </div>
                 </form>
-                <p className="mt-20 text-black">Ya tienes una cuenta?
-                <Link to={`/login-candidato`}>
-                <a href="#!"> Accede</a>
-                </Link>
+                <p className="mt-20 text-black">
+                  Ya tienes una cuenta?
+                  <Link to={`/login-candidato`}>Accede</Link>
                 </p>
-                </div>
               </div>
-            <div className="col-md-6 col-md-offset-3">
-                <div className="block text-center  shadow-none">
-                <img className="container w-100 h-50" src={register} alt=""/>
-                </div>
             </div>
+            <div className="col-md-6 col-md-offset-3">
+              <div className="block text-center  shadow-none">
+                <img className="container w-100 h-50" src={register} alt="" />
+              </div>
             </div>
           </div>
-        </section>
-        
-        </>
-    )
+        </div>
+      </section>
+    </>
+  );
+};
 
-}
-
-export default RegisterCandidate
+export default RegisterCandidate;
--- src/componentes/Recruiter/services/vacancy.js
+++ src/componentes/Recruiter/services/vacancy.js
@@ -1,5 +1,10 @@
 export const endpointsGral={
     vacancyURL:'https://apibackendjob-production.up.railway.app/api/v1/jobVacancy/',
     userURL:'https://apibackendjob-production.up.railway.app/api/v1/users',
-    jobSkill:'https://apibackendjob-production.up.railway.app/api/v1/jobSkill/'
+    registerUser: 'http://localhost:4000/api/v1/signup',
+    jobSkill:'https://apibackendjob-production.up.railway.app/api/v1/jobSkill/',
+    confirmEmail: 'http://localhost:4000/api/v1/signup/sendAccessCode'
 }
+
+
+//registerUser: 'https://apibackendjob-production.up.railway.app/api/v1/signup',

0b57e6f90c4f3828b59a1c2f2f5147cf02a6ae9c
MASC95 <miguelangel.serapio@iest.edu.mx>
2023-06-06 20:50:01 -0600

    home page

--- src/componentes/Candidate/HomeCandidate/HomeCandidate.js
+++ src/componentes/Candidate/HomeCandidate/HomeCandidate.js
@@ -9,7 +9,8 @@ export const HomeCandidate=()=>{
 
             </div>
             <div className="welcome-text">
-            <h1 className="welcome-jobinder d-flex justify-content-center" >BIENVENIDO A JOBINDER</h1>
+            <h1></h1>
+            <h2 className="welcome-jobinder d-flex justify-content-center" >Looking for a Job? Find Your Next Opportunity with Us</h2>
             </div>
             </div>
         </header>
--- src/componentes/Recruiter/services/endpoints.js
+++ src/componentes/Recruiter/services/endpoints.js
@@ -1,7 +1,7 @@
 
 import axios from "axios"
- const loginURL='http://107.23.237.6/api/v1/login'
-//const loginURL='http://localhost:4000/api/v1/login'
+ //const loginURL='http://107.23.237.6/api/v1/login'
+const loginURL='http://localhost:4000/api/v1/login'
 // const vacancyURL='https://apibackendjob-production.up.railway.app/api/v1/jobVacancy'
 
 const loginAxios=async credentials=>{

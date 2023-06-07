77165b67c5b2992d6a1c418ea147e234985695c4
MASC95 <miguelangel.serapio@iest.edu.mx>
2023-06-07 04:50:18 -0600

    jobinder text

--- src/componentes/Candidate/HomeCandidate/HomeCandidate.js
+++ src/componentes/Candidate/HomeCandidate/HomeCandidate.js
@@ -1,19 +1,28 @@
-import React from "react";
-import './homecandidate.scss'
+import React, { useState, useEffect } from "react";
+import './homecandidate.scss';
 
-export const HomeCandidate=()=>{
-    return(
+const HomeCandidate = () => {
+    const [showText, setShowText] = useState(false);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setShowText(true);
+        }, 9000);
+
+        return () => clearTimeout(timer);
+    }, []);
+
+    return (
         <header className="background-image">
             <div className="wrapper">
-            <div className="logo">
-
-            </div>
-            <div className="welcome-text">
-            <h1 className="message"></h1>
-            <h2 className="jobinder-text" data-text='jobinder...' >jobinder...</h2>
-            </div>
+                <div className="logo"></div>
+                <div className="welcome-text">
+                    <h1 className="message"></h1>
+                    {showText && <h2 className="jobinder-text" data-text='jobinder...'>jobinder...</h2>}
+                </div>
             </div>
         </header>
-    )
-}
-export default HomeCandidate
+    );
+};
+
+export default HomeCandidate;

5ea7b643aaf826111ad1110827abd54d8c43bb3b
MASC95 <miguelangel.serapio@iest.edu.mx>
2023-06-06 19:53:20 -0600

    homecandidate

--- src/componentes/Candidate/DashboardCandidate/DashboardCandidate.js
+++ src/componentes/Candidate/DashboardCandidate/DashboardCandidate.js
@@ -18,19 +18,19 @@ export const DashboardCandidate=()=>{
          <div style={isVisibleSidebar?{}:{width:'100vw'}} className={isVisibleSidebar?'dashboard':''}>
             {isVisibleSidebar&& <SidebarCandidate/>}
             <div style={isVisibleSidebar?{}:{width:'100vw'}} className={isVisibleSidebar?'dashboard-app':''}>
-                <header style={isVisibleSidebar?{}:{width:'100vw'}} className={isVisibleSidebar?'dashboard-toolbar': 'w-100 navbar border'}>
+                <header style={isVisibleSidebar?{}:{width:'100vw', backgroundColor: '#E9EDF4'}} className={isVisibleSidebar?'dashboard-toolbar': 'w-100 navbar border'} >
                     
-                    <div className="row profile-container d-flex w-100">
-                        <div className={isVisibleSidebar? "col-sm-3 col-md-3 my-3": "col-sm-3 col-md-3 my-3"} style={{backgroundColor: 'blue'}}>
+                    <div className="row profile-container w-100" >
+                        <div className={isVisibleSidebar? "toggle col-sm-3 col-md-3 my-3": "col-sm-3 col-md-3 my-3"}  >
                             <button onClick={handleIsVisibleSidebar} className="btn btn-light menu-toggle"><FaBars/></button>
                         </div> 
                    
-                    <div className={isVisibleSidebar?"col-sm-6 col-md-7 blankSpaceCenter": "col-sm-6 col-md-7 blankSpaceCenter"} style={{backgroundColor: 'red'}}>
+                    <div className={isVisibleSidebar?"col-sm-6 col-md-7 ": "col-sm-6 col-md-7 blankSpaceCenter"}>
 
                     </div>
-                    <div className={isVisibleSidebar?"col-sm-3 col-md-auto image-container my-3 form-inline d-flex flex-nowrap": "col-sm-3 col-md-auto image-container my-3 form-inline d-flex flex-nowrap "} >
-                        <p className="text-dark">Candidato</p>
-                        <img style={isVisibleSidebar?{}:{width:'50px'}} src={imgProfile} alt="profile-pic"/>
+                    <div className={isVisibleSidebar?"col-sm-3 col-md-auto image-container my-3 form-inline d-flex flex-nowrap": "col-sm-3 col-md-auto image-container my-3 form-inline d-flex flex-nowrap" }>
+                        <p className="candidate-text text-dark d-flex justify-content-center align-items-center" style={{marginRight: '20px'}}>Candidato</p>
+                        <img style={isVisibleSidebar?{}:{width:'50px' , borderRadius:'50%'}} src={imgProfile} alt="profile-pic" className="profile-pic"/>
                     </div>
                 </div>
                   
--- src/componentes/Candidate/HomeCandidate/HomeCandidate.js
+++ src/componentes/Candidate/HomeCandidate/HomeCandidate.js
@@ -1,11 +1,18 @@
 import React from "react";
+import './homecandidate.scss'
 
 export const HomeCandidate=()=>{
     return(
-        <>
+        <header className="background-image">
+            <div className="wrapper">
+            <div className="logo">
 
-            <h1 className="d-flex justify-content-center" >BIENVENIDO A JOBINDER</h1>
-        </>
+            </div>
+            <div className="welcome-text">
+            <h1 className="welcome-jobinder d-flex justify-content-center" >BIENVENIDO A JOBINDER</h1>
+            </div>
+            </div>
+        </header>
     )
 }
 export default HomeCandidate

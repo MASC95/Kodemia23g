3cc5f70565770eb56ce9be0d7ac7e2f254ac42e9
MASC95 <miguelangel.serapio@iest.edu.mx>
2023-06-07 19:32:08 -0600

    color

--- src/componentes/Candidate/DashboardCandidate/DashboardCandidate.js
+++ src/componentes/Candidate/DashboardCandidate/DashboardCandidate.js
@@ -1,58 +1,112 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import SidebarCandidate from "../SidebarCandidate/SidebarCandidate";
 import { FaBars } from "react-icons/fa";
-import imgProfile from '../../Recruiter/assets/img/perfil2.jpg'
+import imgProfile from "../../Recruiter/assets/img/perfil2.jpg";
 import { Outlet } from "react-router-dom";
+import useJob from "../../../hooks/useJob";
 
-export const DashboardCandidate=()=>{
-    const [isVisibleSidebar, setIsVisibleSidebar] = useState(false);
-    const handleIsVisibleSidebar = ()=>{
-        setIsVisibleSidebar(prev=>!prev);
+export const DashboardCandidate = () => {
+  const [isVisibleSidebar, setIsVisibleSidebar] = useState(false);
+  const [dataCandidate] = useJob();
+  const handleIsVisibleSidebar = () => {
+    setIsVisibleSidebar((prev) => !prev);
+  };
+  useEffect =(() =>{
+    if(dataCandidate){
+        console.log('usuario logeado...', dataCandidate)
     }
-  
+    else{
+        console.log('no hay usuario logeado...')
+    }
+  }, [dataCandidate]);
 
-  
-  
-    return(
-        <>
-         <div style={isVisibleSidebar?{}:{width:'100vw'}} className={isVisibleSidebar?'dashboard':''}>
-            {isVisibleSidebar&& <SidebarCandidate/>}
-            <div style={isVisibleSidebar?{}:{width:'100vw'}} className={isVisibleSidebar?'dashboard-app':''}>
-                <header style={isVisibleSidebar?{}:{width:'100vw', backgroundColor: '#E9EDF4'}} className={isVisibleSidebar?'dashboard-toolbar': 'w-100 navbar border'} >
-                    
-                    <div className="row profile-container w-100" >
-                        <div className={isVisibleSidebar? "toggle col-sm-3 col-md-3 my-3": "col-sm-3 col-md-3 my-3"}  >
-                            <button onClick={handleIsVisibleSidebar} className="btn btn-light menu-toggle"><FaBars/></button>
-                        </div> 
-                   
-                    <div className={isVisibleSidebar?"col-sm-6 col-md-7 ": "col-sm-6 col-md-7 blankSpaceCenter"}>
+  return (
+    <>
+      <div
+        style={isVisibleSidebar ? {} : { width: "100vw" }}
+        className={isVisibleSidebar ? "dashboard" : ""}
+      >
+        {isVisibleSidebar && <SidebarCandidate />}
+        <div
+          style={isVisibleSidebar ? {} : { width: "100vw" }}
+          className={isVisibleSidebar ? "dashboard-app" : ""}
+        >
+          <header
+            style={
+              isVisibleSidebar
+                ? {}
+                : { width: "100vw", backgroundColor: "#E9EDF4" }
+            }
+            className={
+              isVisibleSidebar ? "dashboard-toolbar" : "w-100 navbar border"
+            }
+          >
+            <div className="row profile-container w-100">
+              <div
+                className={
+                  isVisibleSidebar
+                    ? "toggle col-sm-3 col-md-3 my-3"
+                    : "col-sm-3 col-md-3 my-3"
+                }
+              >
+                <button
+                  onClick={handleIsVisibleSidebar}
+                  className="btn btn-light menu-toggle"
+                >
+                  <FaBars />
+                </button>
+              </div>
 
-                    </div>
-                    <div className={isVisibleSidebar?"col-sm-3 col-md-auto image-container my-3 form-inline d-flex flex-nowrap": "col-sm-3 col-md-auto image-container my-3 form-inline d-flex flex-nowrap" }>
-                        <p className="candidate-text text-dark d-flex justify-content-center align-items-center" style={{marginRight: '20px'}}>Candidato</p>
-                        <img style={isVisibleSidebar?{}:{width:'50px' , borderRadius:'50%'}} src={imgProfile} alt="profile-pic" className="profile-pic"/>
-                    </div>
-                </div>
-                  
-                    
-                </header>
-                <div className='dashboard-content'>
-                    <div className='container'>
-                        <div className='card'>
-                            {/* <div className='card-header'>
+              <div
+                className={
+                  isVisibleSidebar
+                    ? "col-sm-6 col-md-7 "
+                    : "col-sm-6 col-md-7 blankSpaceCenter"
+                }
+              ></div>
+              <div
+                className={
+                  isVisibleSidebar
+                    ? "col-sm-3 col-md-auto image-container my-3 form-inline d-flex flex-nowrap"
+                    : "col-sm-3 col-md-auto image-container my-3 form-inline d-flex flex-nowrap"
+                }
+              >
+
+                <p
+                  className="candidate-text text-dark d-flex justify-content-center align-items-center"
+                  style={{ marginRight: "20px" }}
+                >
+               
+                </p>
+                <img
+                  style={
+                    isVisibleSidebar
+                      ? {}
+                      : { width: "50px", borderRadius: "50%" }
+                  }
+                  src={imgProfile}
+                  alt="profile-pic"
+                  className="profile-pic"
+                />
+              </div>
+            </div>
+          </header>
+          <div className="dashboard-content">
+            <div className="container">
+              <div className="card">
+                {/* <div className='card-header'>
                                 <h1>BIENVENIDO A JOBINDER</h1>
                             </div> */}
-                            <div className='card-body'>
-                                {/* <p> Usuario Candidato </p> */}
-                                <Outlet />
-                            </div>
-                        </div>
-                    </div>
-                    
+                <div className="card-body">
+                  {/* <p> Usuario Candidato </p> */}
+                  <Outlet />
                 </div>
+              </div>
             </div>
           </div>
-        </>
-    )
-}
-export default DashboardCandidate
+        </div>
+      </div>
+    </>
+  );
+};
+export default DashboardCandidate;

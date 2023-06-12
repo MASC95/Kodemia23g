75dcb4d7bd8242ba753657e8b4ffae22bbeb2760
MASC95 <miguelangel.serapio@iest.edu.mx>
2023-06-12 06:38:27 -0600

    mixin glow

--- src/componentes/Landing/Navbar/Navbar.js
+++ src/componentes/Landing/Navbar/Navbar.js
@@ -5,10 +5,11 @@ import './navbar.scss'
 import logo from '../../Recruiter/assets/img/logo.png'
 const Navbar = () => {
   return (
+    <div className='father-container'>
     <nav className="navbar navbar-expand-lg ">
       <div className="nav-container container-fluid justify-content-between">
         <div className="column-logo col-lg-3">
-          <Link className="link-job navbar-brand me-2 mb-1 d-flex align-items-center" to="/">
+          <Link className="link-job navbar-brand me-2 mb-1 sd-flex align-items-center" to="/">
             <img src={logo} alt='jobinder-logo'className='jobinder-logo' style={{width: "120px ", height: "45px" }} ></img>
           </Link>
         </div>
@@ -35,6 +36,7 @@ const Navbar = () => {
         </ul>
       </div>
     </nav>
+    </div>
   )
 }
 

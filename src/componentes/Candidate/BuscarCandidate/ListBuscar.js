7dfd15253aec959543158741ad9f034fda076d4a
Ramiro Ocampo Rodriguez <101475348+ROR2022@users.noreply.github.com>
2023-06-14 20:28:04 -0600

    devR

--- src/componentes/Candidate/BuscarCandidate/ListBuscar.js
+++ src/componentes/Candidate/BuscarCandidate/ListBuscar.js
@@ -139,7 +139,7 @@ export const ListBuscar = () => {
 
   return (
     <>
-      <VerticalTable/>
+      <VerticalTable vacancies={vacancies} my_vacancies={my_vacancies} handleApply={handleApply} handleStopApplying={handleStopApplying}/>
       <HorizonTable vacancies={vacancies} my_vacancies={my_vacancies} handleApply={handleApply} handleStopApplying={handleStopApplying}/>
       {showAlert && <AlertComponent />}
     </>
--- src/componentes/Candidate/BuscarCandidate/VerticalTable.js
+++ src/componentes/Candidate/BuscarCandidate/VerticalTable.js
@@ -6,7 +6,7 @@ import React from 'react'
 //  Por proyecto    |  Remoto  
 //  Salario         |  Opciones
 // $2500.00         |  Botón
-const VerticalTable = () => {
+const VerticalTable = ({vacancies, my_vacancies, handleStopApplying, handleApply}) => {
   const styleTable ={
     backgroundColor: '#498BA6',
     color:'white',
@@ -17,7 +17,9 @@ const VerticalTable = () => {
   }
   return (
 <div class="d-block d-sm-none">
-  <table class="table">
+{vacancies &&
+                  vacancies?.map((item, index) => (
+  <table class="table mb-5">
     <tbody>
       <tr>
         <th style={styleTable}>#</th>
@@ -41,10 +43,45 @@ const VerticalTable = () => {
       </tr>
       <tr>
         <th>$2500.00</th>
-        <td><button class="btn btn-primary">Abrir</button></td>
+        <td>
+        {my_vacancies?.find(
+                          (myVac) => myVac._id === item._id
+                        ) === undefined ? (
+                          <button
+                            type="submit"
+                            id={item._id}
+                            className="btn btn-outline-info buscar"
+                            onClick={handleApply}
+                            disabled={
+                              my_vacancies?.find(
+                                (myVac) => myVac._id === item._id
+                              ) === undefined
+                                ? false
+                                : true
+                            }
+                          >
+                            {my_vacancies?.find(
+                              (myVac) => myVac._id === item._id
+                            ) === undefined
+                              ? "Aplicar"
+                              : "Aplicando"}
+                          </button>
+                        ) : (
+                          <button
+                            type="submit"
+                            className="btn btn-outline-danger"
+                            id={item._id}
+                            onClick={handleStopApplying}
+                          >
+                            Dejar de aplicar
+                          </button>
+                        )}
+            <button className="btn btn-primary">Abrir</button>
+        </td>
       </tr>
     </tbody>
   </table>
+        ))}
 </div>
 
   )

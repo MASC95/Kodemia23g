dc41e3dbf04487de96eb490ba5c8781c0bf6bbdb
Ramiro Ocampo Rodriguez <101475348+ROR2022@users.noreply.github.com>
2023-06-29 18:13:48 -0600

    versionPrd

--- src/componentes/Recruiter/services/endpoints.js
+++ src/componentes/Recruiter/services/endpoints.js
@@ -1,7 +1,8 @@
 
 import axios from "axios"
+import { backURL } from "../../lib/myLib"
  //const loginURL='http://107.23.237.6/api/v1/login'
-const loginURL='http://localhost:4000/api/v1/login'
+const loginURL=`${backURL}/api/v1/login`;
 const loginAxios=async credentials=>{
     const {data}= await axios.post(loginURL,credentials)
     return data

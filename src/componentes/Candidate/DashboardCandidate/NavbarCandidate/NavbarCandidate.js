f22a003ffd0e8bc8ba270c2984a6c709d48a7ebc
MASC95 <miguelangel.serapio@iest.edu.mx>
2023-07-01 15:34:25 -0600

    navbar

--- src/componentes/Candidate/DashboardCandidate/NavbarCandidate/NavbarCandidate.js
+++ src/componentes/Candidate/DashboardCandidate/NavbarCandidate/NavbarCandidate.js
@@ -27,26 +27,28 @@ const NavbarCandidate = () => {
   };
 
   return (
-    <Navbar expand="lg" className="nav w-100">
+    <Navbar expand="lg" className="nav w-100 c-navbar">
       <Container className="container-fluid contenedor">
-       
         <Navbar.Brand href="#home" className="logo d-flex gap-2">
           {" "}
           <Button
-          variant="primary"
-          onClick={handleShowOffCanvas}
-          className="toggle d-flex justify-content-center align-items-center "
-        >
-          <FaBars />
-        </Button>
-          <img src={logo} alt="Logo" className="logo-img d-flex justify-content-start" />{" "}
+            variant="primary"
+            onClick={handleShowOffCanvas}
+            className="toggle d-flex justify-content-center align-items-center "
+          >
+            <FaBars />
+          </Button>
+          <img
+            src={logo}
+            alt="Logo"
+            className="logo-img d-flex justify-content-start"
+          />{" "}
         </Navbar.Brand>
         <OffCanvasCandidate
           showOffcanvas={showOffcanvas}
           handleShowOffcanvas={handleShowOffCanvas}
         />
 
-        
         <div className="container-m1 d-flex justify-content-end">
           <p className="user-data ">
             {" "}
@@ -58,12 +60,24 @@ const NavbarCandidate = () => {
                 : `${dataCandidate.email}`}
             </span>
           </p>
-          
+
           {dataCandidate.avatar_url ? (
-              <img src={dataCandidate.avatar_url} alt="candidate-profile-pic" className="candidate-profile-pic " />
-            ) : (
-              <FaUserCircle className="candidate-profile-icon" style={{width: '40px', height: '40px', color: '#FFF', marginLeft:'30px'}} />
-            )}
+            <img
+              src={dataCandidate.avatar_url}
+              alt="candidate-profile-pic"
+              className="candidate-profile-pic "
+            />
+          ) : (
+            <FaUserCircle
+              className="candidate-profile-icon"
+              style={{
+                width: "40px",
+                height: "40px",
+                color: "#FFF",
+                marginLeft: "30px",
+              }}
+            />
+          )}
         </div>
       </Container>
     </Navbar>
--- src/componentes/Recruiter/SidebarRecruiter/NavbarRecruiter.js
+++ src/componentes/Recruiter/SidebarRecruiter/NavbarRecruiter.js
@@ -5,37 +5,50 @@ import OffCanvasRecruiter from "./OffCanvasRecruiter/OffCanvarRecruiter";
 import Button from "react-bootstrap/Button";
 import { FaBars } from "react-icons/fa";
 import logo from "../../Recruiter/assets/img/logo.png";
-import useJob from '../../../hooks/useJob'
+import useJob from "../../../hooks/useJob";
 
 function NavbarRecruiter() {
-  const [dataCandidate,setDataCandidate,dataRecruiter,setDataRecruiter, dataLocalStorage, setDataLocalStorage]= useJob();
+  const [
+    dataCandidate,
+    setDataCandidate,
+    dataRecruiter,
+    setDataRecruiter,
+    dataLocalStorage,
+    setDataLocalStorage,
+  ] = useJob();
 
-  const style={
-    width:'150px',
-    height:'50px'
-  }
+  const style = {
+    width: "150px",
+    height: "50px",
+  };
   const [showOffcanvas, setShowOffcanvas] = useState(false);
   const handleShowOffCanvas = () => {
     setShowOffcanvas((prev) => !prev);
   };
   return (
-    <Navbar expand="lg" className="nav">
+    <Navbar expand="lg" className="nav c-navbar">
       <Container className="container">
-      <Button variant="primary" onClick={handleShowOffCanvas} className="toggle bg-transparent border-0">
+        <Button
+          variant="primary"
+          onClick={handleShowOffCanvas}
+          className="toggle bg-transparent border-0"
+        >
           <FaBars />
         </Button>
         <Navbar.Brand href="#home" className="logo ">
           {" "}
-          <img 
-            style={style}
-            src={logo}
-            alt="Logo"
-            className="logo-img"
-          />{" "}
+          <img style={style} src={logo} alt="Logo" className="logo-img" />{" "}
         </Navbar.Brand>
         <div className="col image-container d-flex justify-content-end g-4">
-            <p className="d-none d-sm-block mt-3 text-light" >{`${dataRecruiter.name ? dataRecruiter.name: dataRecruiter.email}`}</p>
-             <img src={dataRecruiter.avatar_url} className="rounded-5 m-2" style={{width:'50px',height:'50px'}} alt=""/>
+          <p className="d-none d-sm-block mt-3 text-light">{`${
+            dataRecruiter.name ? dataRecruiter.name : dataRecruiter.email
+          }`}</p>
+          <img
+            src={dataRecruiter.avatar_url}
+            className="rounded-5 m-2"
+            style={{ width: "50px", height: "50px" }}
+            alt=""
+          />
         </div>
 
         <OffCanvasRecruiter
@@ -46,4 +59,4 @@ function NavbarRecruiter() {
     </Navbar>
   );
 }
-export default NavbarRecruiter
+export default NavbarRecruiter;

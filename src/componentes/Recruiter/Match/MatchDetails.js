4bffbcb08edc42b8b7ee993448d0fdeb6a3fa522
Cinthia <cinthiasanchezr@1997@gmail.com>
2023-07-05 20:23:54 -0700

    add paginate for skills, vacancy, match

--- src/componentes/Recruiter/Match/MatchDetails.js
+++ src/componentes/Recruiter/Match/MatchDetails.js
@@ -1,10 +1,7 @@
 import { useState, useEffect } from "react";
-
-
 import { Link } from "react-router-dom";
 import axios from "axios";
 import { endpointsGral } from "../services/vacancy";
-
 import Swal from "sweetalert2";
 import useJob from "../../../hooks/useJob";
 import "./style.scss";
@@ -26,22 +23,28 @@ export const MatchDetails = () => {
     setDataLocalStorage,
   ] = useJob();
   const [dataByUserCandidate, setDataByUserCandidate] = useState([]);
-  const [dataJobSkills, setDataJobSkills]= useState([]);
+  const [dataInfoVacancy, setDataInfoVacancy]= useState([]);
   const [nuevoListado, setNuevoListado] = useState(false);
+  const [isButtonDisabled, setButtonDisabled] = useState(false);
+  const [buttonState, setButtonState]=useState("btn-outline-success")
+  const [tempListUser, setListDataUser] = useState([]);
 
   const [loading, setLoading] = useState(false);
   const [totalRows, setTotalRows] = useState(0);
   const [perPage, setPerPage] = useState(10);
   const [currentPage, setCurrentPage] = useState(1);
 
+
   const fetchForMatch = async (page,newPerPage) => {
     try {
+      setLoading(true)
       const endpointURL = `${endpointsGral.usersInVacancy}${idVacancy}?page=${page}&limit=${newPerPage}`;
       const response = await axios.get(endpointURL);
-      console.log('responseBackend:..',response);
-      const datasVacancy = response?.data?.item?.docs||[];
-      //setDataJobSkills(datasVacancy?.job_skills);
-      setDataByUserCandidate(datasVacancy);
+      // console.log('responseBackend:..',response);
+      const datasUsers = response?.data?.item?.docs||[];
+      setDataByUserCandidate(datasUsers);
+      setTotalRows(datasUsers["totalDocs"])
+         setLoading(false)
     } catch (error) {
       console.log(error);
     }
@@ -51,25 +54,52 @@ export const MatchDetails = () => {
     listadoApplicants();
   }, []);
 
+  const queryVacancy= async()=>{
+    try {
+      const endpointVacancy=`${endpointsGral.vacancyURL}${idVacancy}`
+      const response= await axios.get(endpointVacancy)
+      const dataVacancy= response?.data?.infoVacancy
+      // console.log('datos vacante', dataVacancy)
+      setDataInfoVacancy(dataVacancy)
+    } catch (error) {
+      
+    }
+  }
+  useEffect(()=>{
+    queryVacancy()
+  },[])
+
+  useEffect(()=>{
+    //console.log('dataSkill(AddSkills):..',dataSkill)
+    if(tempListUser.length>0){
+      console.log('Lista de usuarios para panel:..',tempListUser)
+    }
+  },[tempListUser])
+
+     // pagination
+    const handlePageChange = page => {
+      queryVacancy(page,perPage);
+      setCurrentPage(page)
+    };
+
+    const handlePerRowsChange = async (newPerPage, page) => {
+      console.log('Cambiando limit:...',newPerPage);
+      queryVacancy(page,newPerPage)
+      setPerPage(newPerPage)
+    };
+    // pagination
+
   useEffect(() => {
     console.log("nueva lista de candidatos:..",dataByUserCandidate);
-    if(dataByUserCandidate?.applicants){
+    if(dataByUserCandidate){
       listadoApplicants();
     }
-    
-    
   }, [dataByUserCandidate]);
 
-  const listadoApplicants = ()=>{
-    
+  const listadoApplicants = ()=>{ 
     setNuevoListado(true)
   }
   
-  
-  //console.log("Job", jobSkilss);
-
-  const handleAddPanel = (id) => {};
-
   const dadHandleHideofPanel = (index) => {
       console.log('En el papa(index):',index);
      Swal.fire({
@@ -98,19 +128,58 @@ export const MatchDetails = () => {
       }
     }); 
   };
-  
-
-  // const data = dataShow;
-
-  
-
+  const dadHandleofPanel=(index)=>{
+    Swal.fire({
+      title: "Panel de reclutamiento",
+      text: "Estas seguro de agregar al candidato?!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Si, agregar!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        const selectUser= dataByUserCandidate[index]
+        console.log(selectUser)
+        let tempDataUser=[...tempListUser]
+        const  dataRepet= tempDataUser.some((item)=>item._id===selectUser._id)
+        console.log('data repet', dataRepet)
+        if(dataRepet){
+          console.log('ya esta agregado')
+          Swal.fire(
+            'Acción rechazada!',
+            'Este usuario ya esta agregado al panel!',
+            'error'
+          )
+        }else{
+          tempDataUser.push(selectUser)
+          setListDataUser([...tempDataUser])
+        }
+        // console.log('lista de usuarios para reclutamiento',tempDataUser)
+        setButtonDisabled(true)
+        // setButtonState("btn-outline-secondary")
+        }
+    }); 
+  }
   return (
     <>
       <div className="row m-3">
         <h2 className="text-dark">Lista de aplicantes</h2>
         <div className="col">
           {dataByUserCandidate?.length>0&&
-          <MyTable dadHandleHideofPanel={dadHandleHideofPanel} job_skills={dataJobSkills} dataByUserCandidate={dataByUserCandidate}/>
+          <MyTable 
+          dadHandleHideofPanel={dadHandleHideofPanel} 
+          dadHandleofPanel={dadHandleofPanel}
+          dataInfoVacancy={dataInfoVacancy} 
+          dataByUserCandidate={dataByUserCandidate}
+          handlePageChange={handlePageChange}
+          handlePerRowsChange={handlePerRowsChange}
+          totalRows={totalRows}
+          loading={loading}
+          currentPage={currentPage}
+          isButtonDisabled={isButtonDisabled}
+          buttonState={buttonState}
+         />
           }
         </div>
       </div>

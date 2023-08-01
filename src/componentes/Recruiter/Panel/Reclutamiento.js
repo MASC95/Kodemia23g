dc6ab4914e45770eedb970c9d156f46e5a07b7e7
Ramiro Ocampo Rodriguez <101475348+ROR2022@users.noreply.github.com>
2023-07-31 22:07:31 -0600

    fixing

--- src/componentes/Recruiter/Panel/Reclutamiento.js
+++ src/componentes/Recruiter/Panel/Reclutamiento.js
@@ -274,7 +274,7 @@ try {
               <div
                 className="dd-element"
                 key={myId()}
-                draggable
+                draggable={true}
                 onDragStart={(evt) => startDrag(evt, item)}
               >
                 <p className="body">{item.body}</p>
